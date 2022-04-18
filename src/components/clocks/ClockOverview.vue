8ea664c1df57ba214a9c057d3a3e960678002e07
umfy <39531680+umfy@users.noreply.github.com>
2022-04-18 20:24:10 +0200

    resizing images

--- src/components/clocks/ClockOverview.vue
+++ src/components/clocks/ClockOverview.vue
@@ -1,5 +1,5 @@
 <template>
-  <ion-img :src="image.data" :alt="title"></ion-img>
+  <img :src="image.data" :alt="title" id="photo">
   <h1 class="ion-text-center">{{ title }}</h1>
   <!-- <h2>Opis:</h2> -->
   <ion-item lines="none">
@@ -24,69 +24,77 @@
 </template>
 
 <script>
-import { IonImg, IonItem, IonButton, IonIcon } from "@ionic/vue";
-import { analyticsOutline } from "ionicons/icons";
-import DisplayUneditableDates from "../ui/DisplayUneditableDates.vue";
+import { IonItem, IonButton, IonIcon } from '@ionic/vue'
+import { analyticsOutline } from 'ionicons/icons'
+import DisplayUneditableDates from '../ui/DisplayUneditableDates.vue'
+import PinchZoom from 'pinch-zoom-js'
 // import BaseCard from "../base/BaseCard.vue";
-import BaseBadge from "../base/BaseBadge.vue";
+import BaseBadge from '../base/BaseBadge.vue'
 export default {
   props: {
     title: String,
     image: Object,
     description: String,
-    history: Array
+    history: Array,
   },
   components: {
     DisplayUneditableDates,
     BaseBadge,
     // BaseCard,
-    IonImg,
     IonItem,
     IonButton,
-    IonIcon
+    IonIcon,
   },
   data() {
     return {
-      sentence: "Zegar jest zatrzymany.",
-      sentenceDays: "",
-      analyticsOutline
-    };
+      sentence: 'Zegar jest zatrzymany.',
+      sentenceDays: '',
+      analyticsOutline,
+    }
   },
   computed: {
     countDays() {
-      let stopDate = "";
+      let stopDate = ''
       if (
         this.history == undefined ||
         this.history.length === 0 ||
-        this.history[0].start == ""
+        this.history[0].start == ''
       ) {
-        this.setSentence("Zegar jest zatrzymany.", "");
-        return "-";
+        this.setSentence('Zegar jest zatrzymany.', '')
+        return '-'
       }
 
-      if (this.history[0].stop == "") {
-        this.setSentence("Działa nieprzerwanie od", "dni.");
-        stopDate = new Date();
+      if (this.history[0].stop == '') {
+        this.setSentence('Działa nieprzerwanie od', 'dni.')
+        stopDate = new Date()
       } else {
-        this.setSentence("Zegar działał", "dni.");
-        stopDate = new Date(this.history[0].stop);
+        this.setSentence('Zegar działał', 'dni.')
+        stopDate = new Date(this.history[0].stop)
       }
-      const startDate = new Date(this.history[0].start);
+      const startDate = new Date(this.history[0].start)
       // parseInt was bad idea. It took numbers like 6^-10 and parsed it to 6 instead of 0
-      return Math.floor((stopDate - startDate) / 86400000);
-    }
+      return Math.floor((stopDate - startDate) / 86400000)
+    },
   },
   methods: {
     setSentence(one, two) {
-      this.sentence = one;
-      this.sentenceDays = two;
+      this.sentence = one
+      this.sentenceDays = two
     },
     analyticsRedirect() {
-      this.$router.push(this.$route.path + "/analytics");
-    }
+      this.$router.push(this.$route.path + '/analytics')
+    },
   },
-  created() {}
-};
+  mounted() {
+    const myElement = document.getElementById('photo')
+    setTimeout(() => {
+      new PinchZoom(myElement, {
+        draggableUnzoomed: false,
+        minZoom: 1,
+      })
+    }, 50);
+  },
+}
 </script>
 
 <style scoped>
@@ -105,5 +113,8 @@ h3 {
 h4 {
   font-size: 1.2rem;
   color: #bf3100;
+}
+#photo{
+max-height: 100vh;
 }
 </style>
